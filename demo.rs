89cce5b5717b2f924111050240a942de5054b429
YourAverageLink <37936141+YourAverageLink@users.noreply.github.com>
2023-02-17 12:09:58 -0600

    Add alternate version
    Slight variant of the algorithm that uses a logically equivalent loop condition to avoid computing !lo multiple times

--- demo.rs
+++ demo.rs
@@ -1,4 +1,7 @@
 // This will not compile, this is just a snippet.
+// TODO: Maybe making result mutable and += (hi > !lo) as u64
+// and lo = r.wrapping_mul(n) * (hi == lo) as u64 might be faster
+// because there'd be no branches?
 fn gen_mod_u64(n: u64) -> u64 {
         // Code snippet adapted from smol-rs/fastrand, which was itself adapted from Lemire's algorithm.
         let mut r = gen_u64();
@@ -18,3 +21,26 @@ fn gen_mod_u64(n: u64) -> u64 {
         }
         result
     }
+
+// I'll have to benchmark this variant as well, it avoids calculating
+// !lo multiple times, but has a bit more complicated loop condition
+fn alt_mod_u64(n: u64) -> u64 {
+        // Code snippet adapted from smol-rs/fastrand, which was itself adapted from Lemire's algorithm.
+        let mut r = gen_u64();
+        let result = mul_high_u64(r, n);
+        let mut hi: u64;
+        let mut not_lo = !r.wrapping_mul(n);
+        while not_lo < (n - 1) {
+            r = gen_u64();
+            hi = mul_high_u64(r, n);
+            if hi > not_lo {
+              return result + 1;
+            }
+            if hi < not_lo {
+              return result;
+            }
+            not_lo = !r.wrapping_mul(n);
+        }
+        result
+    }
+
