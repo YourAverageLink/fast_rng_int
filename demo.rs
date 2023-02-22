151e468ef20b69ae30cd381b71989b5d8050f4c2
YourAverageLink <37936141+YourAverageLink@users.noreply.github.com>
2023-02-22 13:43:13 -0600

    Update demo.rs

--- demo.rs
+++ demo.rs
@@ -1,23 +1,19 @@
 // This will not compile, this is just a snippet.
-// TODO: Maybe making result mutable and += (hi > !lo) as u64
-// and lo = r.wrapping_mul(n) * (hi == lo) as u64 might be faster
-// because there'd be no branches?
 fn gen_mod_u64(n: u64) -> u64 {
         // Code snippet adapted from smol-rs/fastrand, which was itself adapted from Lemire's algorithm.
         let mut r = gen_u64();
-        let result = mul_high_u64(r, n);
+        let mut result = mul_high_u64(r, n);
         let mut hi: u64;
         let mut lo = r.wrapping_mul(n);
+        // Determine if the number is "binary borderline", that is, extra bits could increment the result
         while lo > n.wrapping_neg() {
             r = gen_u64();
             hi = mul_high_u64(r, n);
-            if hi > !lo {
-              return result + 1;
-            }
-            if hi < !lo {
-              return result;
-            }
-            lo = r.wrapping_mul(n);
+            // hi > !lo implies the new bits *would* increment the original result, so add to the result
+            result += (hi > !lo);
+            // hi != lo implies the new bits definitely do (hi > !lo) or do not (hi < !lo) increment the result
+            // set lo to 0 to exit the loop branchlessly if so.
+            lo = r.wrapping_mul(n) * (hi == !lo) as u64;
         }
         result
     }
@@ -27,19 +23,14 @@ fn gen_mod_u64(n: u64) -> u64 {
 fn alt_mod_u64(n: u64) -> u64 {
         // Code snippet adapted from smol-rs/fastrand, which was itself adapted from Lemire's algorithm.
         let mut r = gen_u64();
-        let result = mul_high_u64(r, n);
+        let mut result = mul_high_u64(r, n);
         let mut hi: u64;
         let mut not_lo = !r.wrapping_mul(n);
         while not_lo < (n - 1) {
             r = gen_u64();
             hi = mul_high_u64(r, n);
-            if hi > not_lo {
-              return result + 1;
-            }
-            if hi < not_lo {
-              return result;
-            }
-            not_lo = !r.wrapping_mul(n);
+            result += (hi > not_lo) as u64;
+            not_lo = !r.wrapping_mul(n) * (hi == not_lo) as u64;
         }
         result
     }
